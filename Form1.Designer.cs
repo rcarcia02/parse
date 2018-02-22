27f24e5a11f3e33839b43caaeb90420c2e35442e
rcarcia02 <rcarcia02@gmail.com>
2018-02-22 14:17:09 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Parse
+{
+    partial class frmChooseFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(200, 59);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.Text = "Choose File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 128);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(124, 13);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "This is just for debugging";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(451, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // frmChooseFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 350);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnFile);
+            this.Name = "frmChooseFile";
+            this.Text = "Choose File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parse
+{
+    public partial class frmChooseFile : Form
+    {
+        public frmChooseFile()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog box.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string fileName = openFileDialog1.FileName; // Will be in form M://...etc.
+
+                using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
+                {
+                    string text = sr.ReadToEnd();
+                    string[] lines = text.Split(' ');
+                    foreach (string s in lines)
+                    {
+                        listBox1.Items.Add(s);
+                    }
+                }
+            }
+        }
+    }
+}
